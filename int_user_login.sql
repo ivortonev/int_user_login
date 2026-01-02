196303923695cdfbd6d1cb15faa418dbcc07f0d4
Ivo Tonev <ivo@tonev.pro.br>
2026-01-02 17:59:14 -0300

    Update int_user_login.sql

--- int_user_login.sql
+++ int_user_login.sql
@@ -1,6 +1,27 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3-1.el10_2.alma_altarch
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 02, 2026 at 08:58 PM
+-- Server version: 10.11.15-MariaDB
+-- PHP Version: 8.3.26
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `int_user_login`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `login`
@@ -9,15 +30,24 @@ SET time_zone = "+00:00";
 CREATE TABLE `login` (
   `time` int(16) NOT NULL,
   `login` varchar(20) NOT NULL,
-  `source_ip` varchar(42) NOT NULL
+  `source_ip` varchar(42) NOT NULL,
+  `expire_time` int(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
-COMMIT;
+
+--
+-- Indexes for dumped tables
+--
 
 --
 -- Indexes for table `login`
 --
-
 ALTER TABLE `login`
-  ADD KEY `time` (`time`),
-  ADD KEY `login` (`login`);
+  ADD UNIQUE KEY `source_ip` (`source_ip`),
+  ADD KEY `indx_expire_time` (`expire_time`),
+  ADD KEY `indx_time` (`time`) USING BTREE,
+  ADD KEY `indx_login` (`login`) USING BTREE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
